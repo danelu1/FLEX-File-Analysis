26fca015be4af780253b6ee0501c35f0cf60541a
danelu1 <111453837+danelu1@users.noreply.github.com>
2023-12-23 00:50:52 +0200

    Add files via upload

--- Varianta C/BikeTest.java
+++ Varianta C/BikeTest.java
@@ -1,6 +1,7 @@
 class Bike {
 
-    private String serialNumber; private String model;
+    private String serialNumber;
+    private String model;
 
     private double price;
 
@@ -8,21 +9,27 @@ class Bike {
         this.model = model; this.price = price;
     }
 
-    public String getSerialNumber() { return serialNumber;
+    public String getSerialNumber() {
+        return serialNumber;
     }
-    public void setSerialNumber(String serialNumber) { this.serialNumber = serialNumber;
-    }
-
-    public String getModel() { return model;
-    }
-
-    public void setModel(String model) { this.model = model;
+    public void setSerialNumber(String serialNumber) {
+        this.serialNumber = serialNumber;
     }
 
-    public double getPrice() { return price;
+    public String getModel() {
+        return model;
     }
 
-    public void setPrice(double price) { this.price = price;
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
     }
 }
 
@@ -33,28 +40,30 @@ class BikeFactory {
 
     private Bike[] bikesArr;
 
-    public BikeFactory(int capacity){ this.bikesArr = new Bike[capacity]; this.totalBikesProduced = 0;
+    public BikeFactory(int capacity){
+        this.bikesArr = new Bike[capacity]; this.totalBikesProduced = 0;
     }
 
 
-    public int getTotalBikesProduced() { return totalBikesProduced;
+    public int getTotalBikesProduced() {
+        return totalBikesProduced;
     }
 
-    public void setTotalBikesProduced(int totalBikesProduced) { this.totalBikesProduced = totalBikesProduced;
+    public void setTotalBikesProduced(int totalBikesProduced) {
+        this.totalBikesProduced = totalBikesProduced;
     }
 
-    public void produceBike(String model, double price){ if(getTotalBikesProduced() < (365*12/1.5)) {
-        setTotalBikesProduced(getTotalBikesProduced() + 1); int b = getTotalBikesProduced() - 1;
-        this.bikesArr[b] = new Bike("bike_" + b, model, price);
-    }
-    else System.out.println("Sorry, too many bikes!");
+    public void produceBike(String model, double price){
+        if(getTotalBikesProduced() < (365*12/1.5)) {
+            setTotalBikesProduced(getTotalBikesProduced() + 1); int b = getTotalBikesProduced() - 1;
+            this.bikesArr[b] = new Bike("bike_" + b, model, price);
+        }
+        else System.out.println("Sorry, too many bikes!");
     }
 }
 public class BikeTest {
-    public static void main(String[] args) { int bikesNo;
+    public static void main(String[] args) {
+        int bikesNo;
         BikeFactory factory = new BikeFactory(10);
-
     }
-
 }
-
