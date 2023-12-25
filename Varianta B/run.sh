c4f6d20f1979f747f0391e42fde833cc8ff0a8aa
danelu1 <111453837+danelu1@users.noreply.github.com>
2023-12-25 15:53:14 +0200

    Add files via upload

---
+++ Varianta B/run.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+tests="./tests"
+out_dirr="./out"
+
+mkdir "$out_dirr"
+
+make
+
+if [ $? -eq 0 ]; then
+	i=0
+
+	for file in "$tests"/*; do
+		if [ -f "$file" ]; then
+			./tema "$file" > "$out_dirr/$i.out"
+			((i++))
+		fi
+	done
+fi
